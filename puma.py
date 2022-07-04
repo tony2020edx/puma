6c97c8f76ae0e9e732ed2c1ab0616cefdecef10e
tony2020edx <tonypauledx1@gmail.com>
2022-07-04 18:03:00 +0530

    next

--- puma.py
+++ puma.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import pandas as pd
 
 start_url = "https://in.puma.com/in/en/collections/collections-football/collections-football-manchester-city-fc"
 
